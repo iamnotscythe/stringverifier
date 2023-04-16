7d4242b2275bf2755883f1f78796194c439e2368
schyte <58274985+iamnotscythe@users.noreply.github.com>
2023-04-16 11:45:52 +0300

    Create strverify.py

---
+++ strverify.py
@@ -0,0 +1,12 @@
+#sha256 gibi degerleri dogrulamak icin kullanilabilecek cok basit bir uygulama
+
+sayi1 = str(input("Birinci sayıyı girin: ")) #ilk deger
+sayi2 = str(input("İkinci sayıyı girin: ")) #ikinci deger
+
+if sayi1 == sayi2:
+    print("Girilen sayılar birbirine eşittir.") #esit oldugunu gosteriyo
+else:
+    print("Girilen sayılar birbirine eşit değildir.") #esit degilmis
+
+
+
